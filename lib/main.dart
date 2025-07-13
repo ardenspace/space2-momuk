a8c4c53bfbbdcf54147fccc20a848ec6e163e37f
arden LEE <ardensdevspace@gmail.com>
2025-07-13 18:09:04 +0900

    [DESIGN] set splash & icon again ....

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: Text('Hello, world!')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // 가운데 정렬
+            children: [
+              const Text('Hello, world!'),
+              const SizedBox(height: 20), // 텍스트와 버튼 사이 간격
+              ElevatedButton(
+                onPressed: () {
+                  // 버튼 클릭 시 실행할 코드
+                  print('Button pressed!');
+                },
+                child: const Text('Click me'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
